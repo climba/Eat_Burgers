98c08f445e82d5e97f38f98d7ad737cd8ffbb89f
climba <mike@joomla-go.com>
2018-06-06 09:59:03 -0700

    Changed heroku confs

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "DATABASE_URL",
+  host: "y2w3wxldca8enczv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "",
-  database: "postgresql-fluffy-25188"
+  user: "vk0dwomusju5coz7",
+  password: "rtnul2v16euax9qx",
+  database: "f0augg0ycswubvc7"
 });
 
 // Make connection.
