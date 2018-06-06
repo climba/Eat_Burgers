1a2751f63da9be1d6a3b659b33844d1125f9d86b
climba <mike@joomla-go.com>
2018-06-05 18:19:47 -0700

    Added main DIR structure

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (name) VALUES ('NO. 1 Farm Burger');
+INSERT INTO burgers (name) VALUES ('NO. 2 Vegan Burger');
+INSERT INTO burgers (name, eaten) VALUES ('NO. 3 Pastured Pig', true);
+INSERT INTO burgers (name, eaten) VALUES ('NO. 4 Chicken Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('NO. 5 Sunny Side', true);
+INSERT INTO burgers (name) VALUES ('NO. 6 Bacon Burger');
+INSERT INTO burgers (name) VALUES ('NO. 7 Impossible Burger');
