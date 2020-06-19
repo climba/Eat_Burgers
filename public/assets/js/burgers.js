e07edc92014aa2babdf69543a5c8101183a941e0
climba <mike@joomla-go.com>
2020-06-19 12:47:16 -0700

    Fixed working tree

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,7 +2,7 @@
 $(function() {
   $(".change-eaten").on("click", function(event) {
     var id = $(this).data("id");
-    var newEaten = $(this).data("newEaten");
+    var newEaten = $(this).data("neweat");
 
     var newEatenState = {
       eaten: newEaten
--- server.js
+++ server.js
@@ -1,29 +1,32 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
+var PORT = process.env.PORT || 8080;
 
-var apiRoutes = require("./app/routing/apiRoutes.js");
-var htmlRoutes = require("./app/routing/htmlRoutes.js");
-
-// Sets up the Express App
-// =============================================================
-// var app = express();
 var app = express();
-// var PORT = 3000; // Local machine  conf
-var PORT = process.env.PORT || 3000; // heroku conf
 
-// Sets up the Express app to handle data parsing
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
 app.use(bodyParser.json());
 
-app.use('/', htmlRoutes);
-app.use('/api', apiRoutes);
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgersController.js");
 
-// Starts the server to begin listening
-// =============================================================
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
